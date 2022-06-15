5f1c84fab42bb413dda00e1e12cf930c394d49cf
Emiliano <emilianofuchs@gmail.com>
2022-06-15 16:53:01 -0300

    first commit

---
+++ src/main/java/br/dev/emiliano/fourintents/RecoveryPassword.kt
@@ -0,0 +1,11 @@
+package br.dev.emiliano.fourintents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecoveryPassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recovery_password)
+    }
+}
